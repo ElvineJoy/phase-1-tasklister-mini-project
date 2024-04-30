b86bcf08ff8052e434c950715a6a438dbd23c605
ElvineJoy <elvinkuiyungest@gmail.com>
2024-04-30 13:58:13 +0300

    Delete and add task completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+    
+  const taskForm =document.querySelector('form')
+  
+  taskForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const taskToDo = document.getElementById('new-task-description').value
+     
+    addTasks(taskToDo);
+
+     taskForm.reset();
+  })
 });
+
+
+function addTasks(taskToDo) {
+  const actionList = document.createElement('li');
+  const deleteButton = document.createElement('button');
+
+  deleteButton.addEventListener('click', deleteAction)
+  deleteButton.textContent =  'delete'
+  actionList.textContent = `${taskToDo}   `;
+  actionList.appendChild(deleteButton);
+
+  document.querySelector("#tasks").appendChild(actionList);
+ 
+}
+
+function deleteAction (e) {
+  e.target.parentNode.remove()
+}
